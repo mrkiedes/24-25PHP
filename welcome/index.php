ee1ce37ef4ce3c9ea6a023097f9e5c12eaf7b587
mrkiedes <akiedes@vtsd.com>
2025-01-15 07:58:17 -0500

    Welcome Page Update

--- welcome/index.php
+++ welcome/index.php
@@ -1,11 +1,15 @@
 <?php include 'header.php'; ?>
 
+<form action="welcome.php" method="post">
+<div class="row">
+    <div class="col">
+        Name: <input name="name" type="text" /><br />
+        Email: <input name="email" type="text" /><br />
+        <input type="submit" />
 
-
-
-
-
-
+    </div>
+</div>
+</form>
 
 
 <?php include 'footer.php' ?>
---
+++ welcome/welcome.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<div class="row">
+    <div class="col">
+        <p>Welcome <?php echo ($_POST["name"]); ?>,
+            your email address is <?php echo ($_POST["email"]); ?>.</p>
+    </div>
+</div>
+
+
+
+
+
+<?php include 'footer.php' ?>
