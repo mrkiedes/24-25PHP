82eb99ae1377ae8d6ff59e0003d4fbfffeb39024
mrkiedes <akiedes@vtsd.com>
2025-01-15 07:43:21 -0500

    Welcome Page Update

---
+++ welcome/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome Form</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="mt-4 p-5 bg-primary text-white rounded">
+                <h1>Welcome to the Website</h1>
+                <p>The first form we made!</p>
+            </div>
+        </div>
+    </div>
