927d48b489b6e994a257ba38fc9685294befac0b
mrkiedes <akiedes@vtsd.com>
2025-01-17 09:23:38 -0500

    Sanitize Inputs Update

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,9 +1,31 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+
+// Set up our variables and set them to empty
+$name = $email = "";
+
+// Condition to detect form data and sanitize it
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = sanitize_inputs($_POST["name"]);
+    $email = sanitize_inputs($_POST["email"]);
+}
+// Function to sanitize the data
+function sanitize_inputs($data){
+    // Trim method removes spaces at beginning and end
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+?>
+
+
 
 <div class="row">
     <div class="col">
-        <p>Welcome <?php echo ($_POST["name"]); ?>,
-            your email address is <?php echo ($_POST["email"]); ?>.</p>
+        <p>Welcome <?php echo $name; ?>,
+            your email address is <?php echo $email; ?>.</p>
     </div>
 </div>
 
